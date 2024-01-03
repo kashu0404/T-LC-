375ff2465992116670454f8def9e8adc0794c74d
Kashmala Fareed <109161404+kashu0404@users.noreply.github.com>
2024-01-03 17:28:39 -0500

    Update frontendScript.js

--- GDSC Hackathon/scripts/frontendScript.js
+++ GDSC Hackathon/scripts/frontendScript.js
@@ -54,41 +54,21 @@ function changeMainImage(src) {
         localStorage.setItem('smallImg', JSON.stringify(selectedSmallImg));
     }
 }
-// function displaySelection() {
-//     let titleElements = document.querySelectorAll('.title');
 
-//     titleElements.forEach(titleElement => {
-//         let rangeElements = document.querySelectorAll('.ranges');
-
-//         titleElement.addEventListener('click', function () {
-//             rangeElements.forEach(rangeElement => {
-//                 // Toggle the 'ranges-selected' class
-//                 rangeElement.classList.toggle('ranges-selected');
-
-//                 // Toggle the 'display' property based on the presence of 'ranges-selected' class
-//                 if (rangeElement.classList.contains('ranges-selected')) {
-//                     rangeElement.style.display = 'flex';
-//                 } else {
-//                     rangeElement.style.display = 'none';
-//                 }
-//             });
-//         });
-//     });
-// }
 
 
 function displaySelection(button) {
-    // Get the parent filter container
+
     let filterContainer = button.closest('.filter-container');
 
-    // Hide all ranges in the parent container first
+
     let allRanges = filterContainer.querySelectorAll('.ranges');
     allRanges.forEach(function (range) {
         range.style.display = 'none';
         range.classList.remove('ranges-selected'); // Remove the class from all ranges
     });
 
-    // Show the specific ranges associated with the clicked button
+
     let ranges = button.parentElement.nextElementSibling;
 
     // Toggle the display property of the ranges
