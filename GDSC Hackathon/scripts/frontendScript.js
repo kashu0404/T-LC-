4d9dc241398725827adb2be3b4fbb8ea9e1c43bf
kashu0404 <fareedkashu60@gmail.com>
2024-01-12 12:44:15 -0500

    update

--- GDSC Hackathon/scripts/frontendScript.js
+++ GDSC Hackathon/scripts/frontendScript.js
@@ -87,4 +87,14 @@ function displaySelection(button) {
     }
 }
 
+// faq page
+function toggleAnswer(id) {
+    var answer = document.getElementById(id);
+    if (answer.style.display === 'none') {
+        answer.style.display = 'block';
+    } else {
+        answer.style.display = 'none';
+    }
+}
+
 
